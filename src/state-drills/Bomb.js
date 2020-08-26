2983f45926d7876aa405a7155fc243b4f0c987b1
Mark Marcello <SwiftTactics@gmail.com>
2020-08-26 14:42:28 -0400

    error

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 class Bomb extends Component {
     constructor(props) {
         super(props);
-        this.state = { count: 0 }
+        this.state = { 
+            count: 0,
+            disp: 'str'
+        }
     }
+
+    
 
     componentDidMount(){
         
@@ -12,7 +17,8 @@ class Bomb extends Component {
         this.interval = setInterval(()=> {
             this.setState(({count}) => ({ count: this.state.count+ 1 }))
         }, 1000)
-        console.log(this.state)
+  
+
     }
     render() { 
         return ( <div><p>{this.state.count}</p></div> );
