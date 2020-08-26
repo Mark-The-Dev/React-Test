c79ba2ac3a33c0d2c2271e8da08944b6eccfc3c4
Xavier Ritch <xritch89@hotmail.com>
2020-08-26 14:40:24 -0400

    step 2 complete

--- src/state-drills/Bomb.js
+++ src/state-drills/Bomb.js
@@ -3,23 +3,37 @@ import React, { Component } from 'react'
 class Bomb extends Component {
     constructor(props) {
         super(props);
-        this.state = { count: 0 }
+        this.state = { 
+            count: 0,
+         }
     }
 
     componentDidMount(){
         
-
-         var intervalId = setInterval(this.state.count, 1000);
-         if(newCount >= 0) { 
-            this.setState({ count: newCount });
-        }
         this.interval = setInterval(()=> {
             this.setState(({count}) => ({ count: this.state.count+ 1 }))
         }, 1000)
-        console.log(this.state)
+        console.log(this.state.count)
     }
+
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+
+    display(){
+        const {count} = this.state
+        if(count >= 8){
+            clearInterval(this.interval)
+            return "BOOM!"
+        } else if (count % 2 === 0){
+            return "tick"
+        }else {
+            return "tock"
+        }
+    }
+
     render() { 
-        return ( <div><p>{this.state.count}</p></div> );
+        return ( <div><p>{this.display()}</p></div> );
     }
 }
  
